2ab79b37e36863158db1832e17cbc2082a4ef420
Sriram V <xsriram@gmail.com>
2020-04-01 00:15:19 -0400

    Changes committed - April 1st 2020

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hello-my-world';
+  title1 = "i love coding in Angular";
+}
