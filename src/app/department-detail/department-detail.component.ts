e6cefc31f23c2a45280a6bc00109440723d9c8f4
Sriram V <xsriram@gmail.com>
2020-04-20 00:43:12 -0400

    Added code for ParamMap forward and backward navigation

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import  {ActivatedRoute }  from '@angular/router';
+import  {Router, ActivatedRoute,ParamMap }  from '@angular/router';
+//import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-department-detail',
   template: `
     you selected department with id = {{departmentId}}
-    
-  `
-    ,
+    <br>
 
+    <a (click) = "goPrevious()">Previous</a> &nbsp;
+    <a (click) = "goNext()">Next</a>
   
-  styles: []
+  `,
+
+    styles: []
 })
 export class DepartmentDetailComponent implements OnInit {
   public departmentId;
   //displayName = "false";
-  constructor(private route : ActivatedRoute) { }
+  constructor(private route : ActivatedRoute, private router : Router) { }
 
   ngOnInit(): void {
-      let id = parseInt(this.route.snapshot.paramMap.get('id'));
-      this.departmentId = id;
+      //let id = parseInt(this.route.snapshot.paramMap.get('id'));
+      this.route.paramMap.subscribe((params : ParamMap) => {
+        let id = parseInt(params.get('id'));
+        this.departmentId = id;
+      });
+      
+  }
+
+  goPrevious()
+  {
+     let previousID = this.departmentId - 1;
+     this.router.navigate(['/departments',previousID]);
+  }
+
+  goNext()
+  {
+    let nextID = this.departmentId + 1;
+    this.router.navigate(['/departments',nextID]);
   }
 
 }
