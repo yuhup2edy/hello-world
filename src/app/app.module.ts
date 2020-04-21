7c60198fbc9360b646c0943ce0157b5c5fd307e3
Sriram V <xsriram@gmail.com>
2020-04-21 00:45:07 -0400

    Angular 6 related

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 import { DepartmentContactComponent } from './department-contact/department-contact.component';
 import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 //import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 //import { DepartmentListComponent } from './department-list/department-list.component';
 //import { EmployeeListComponent } from './employee-list/employee-list.component';
@@ -16,14 +24,22 @@ import { DepartmentOverviewComponent } from './department-overview/department-ov
     routingComponents,
     DepartmentDetailComponent,
     DepartmentContactComponent,
-    DepartmentOverviewComponent
+    DepartmentOverviewComponent,
+    MyNavComponent
   //PageNotFoundComponent
   //  DepartmentListComponent,
   //  EmployeeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
